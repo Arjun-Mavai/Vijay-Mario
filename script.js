6be77ac5bbd032441e91b7c103d4587a8bc97601
Arjun Mavai <yuyureka212@gmail.com>
2023-09-09 11:05:25 +0530

    after adding mobile version

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-
-console.log('script file is runnig well : Start')
+console.log('script file is running well: Start')
 
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 const heading = document.querySelector(".game");
 
-
 console.log(' before jump started executing');
 
-
 const jump = () => {
-    console.log('jump started executing');
-
+  console.log('jump started executing');
 
   mario.classList.add("jump");
   setTimeout(() => {
@@ -19,42 +15,40 @@ const jump = () => {
   }, 500);
 };
 
-
 console.log(' after jump started executing');
 
-const loop = setInterval(() => {
+const checkCollision = () => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", " ");
 
-    console.log('loop started executing');
-  
-    console.log(loop);
+  console.log(marioPosition);
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window
-      .getComputedStyle(mario)
-      .bottom.replace("px", " ");
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
 
-    console.log(marioPosition);
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+    heading.textContent = 'Game over - Arjun '
 
-    if (pipePosition <= 120 &&pipePosition>0 && marioPosition<80  ) {
-      pipe.style.animation = "none";
-      pipe.style.left = `${pipePosition}px`;
+    mario.src = "./images/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "50px";
 
-      mario.style.animation = "none";
-      mario.style.bottom = `${marioPosition}px`;
-      heading.textContent ='Game over-Arjun '
-       
+    clearInterval(loop);
+  }
+};
 
-      mario.src = "./images/game-over.png";
-      mario.style.width = "75px";
-      mario.style.marginLeft = "50px";
+const loop = setInterval(checkCollision, 10);
 
-clearInterval(loop)
+document.addEventListener("keydown", (event) => {
+  if (event.key === " ") { // Respond to spacebar key
+    jump();
+  }
+});
 
-    }
-  }, 10);
+document.addEventListener("click", jump); // Respond to mouse click (desktop) and tap (mobile)
 
-
-document.addEventListener("keydown", jump);
-
-
-console.log('script file is runnig well : End')
+console.log('script file is running well: End')
